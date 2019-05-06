2cf7244c5b2c9ba44acd483f14af983f8a82ac98
kristinau09 <kristinau09@gmail.com>
2019-05-05 22:49:14 -0500

    adding files

---
+++ binReader.h
@@ -0,0 +1,42 @@
+/*
+ * Filename        binReader.h
+ * Date            05/04/2019
+ * Author          Kristina Upadhaya
+ * Email           kxu160730@utdallas.edu
+ * Course          CS 3377.502
+ * Version         1.0
+ * Copyright 2019, All Rights Reserved
+ * 
+ * Description: header file
+ * 
+ */
+
+   #include <map>
+   #include <string>
+   #include <stdlib.h>
+   #include <bits/stdc++.h>
+   #include <stdint.h>
+   using namespace std;
+    const int maxStr=20;
+    extern map<int,string> binMap;
+    void binParser(); 
+ 
+ 
+   class Header
+   {
+    public:
+   
+    uint32_t magicNumber; /*Should be OxFEEDFACE */
+    uint32_t versionNumber;
+    uint64_t numRecords;
+   };
+
+  class Record
+  {
+    public:
+  
+    uint8_t strLength;
+    char stringBuffer[maxStr];
+  };
+  
+  
